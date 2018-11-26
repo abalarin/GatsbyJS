fa9314a5bed34e2fd83b40a1b038280ea60685d9
System Administrator <abalarin@ramapo.edu>
2018-11-26 07:15:19 -0500

    mongodb changes

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -12,24 +12,18 @@ import Layout from "../components/layout"
 //
 // // console.log(data.mongodbTestdbUsers)
 
-// export const query = graphql`
-//  query {
-//     allMongodbTestdbUsers {
-//       edges {
-//         node {
-//           id
-//           username
-//         }
-//       }
-//     }
-//   }
-// `
-// <ul>
-//   <h3>Users</h3>
-//   {data.allMongodbTestdbUsers.edges.map(({ node }, index) => (
-//     <li>{node.username}</li>
-//   ))}
-// </ul>
+export const query = graphql`
+ query {
+    allMongodbTestdbUsers {
+      edges {
+        node {
+          id
+          username
+        }
+      }
+    }
+  }
+`
 
 export default ({ data }) => {
   console.log(data)
@@ -38,6 +32,12 @@ export default ({ data }) => {
       <div><img src="https://dogecoin.com/imgs/doge.png" alt="Smiley face"/></div>
       <p>Such wow. Very <Link to="//">React</Link>.</p>
 
+      <ul>
+        <h3>Users</h3>
+        {data.allMongodbTestdbUsers.edges.map(({ node }, index) => (
+          <li>{node.username}</li>
+        ))}
+      </ul>
     </Layout>
   )
 
