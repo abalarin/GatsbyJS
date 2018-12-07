89c00a32286c65a6f36d932f15007bef84a9ef1f
System Administrator <abalarin@ramapo.edu>
2018-12-07 07:40:31 -0500

    mongo....

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, graphql} from "gatsby"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 
 // export const query = graphql`
@@ -12,31 +12,31 @@ import Layout from "../components/layout"
 
 // console.log(data.mongodbTestdbUsers)
 
-export const query = graphql`
- query {
-    allMongodbTestdbUsers {
-      edges {
-        node {
-          id
-          username
-        }
-      }
-    }
-  }
-`
-
+// export const query = graphql`
+//  query {
+//     allMongodbTestdbUsers {
+//       edges {
+//         node {
+//           id
+//           username
+//         }
+//       }
+//     }
+//   }
+// `
+// <ul>
+//   <h3>Users</h3>
+//   {data.allMongodbTestdbUsers.edges.map(({ node }, index) => (
+//     <li>{node.username}</li>
+//   ))}
+// </ul>
 export default ({ data }) => {
   console.log(data)
   return (
     <Layout>
       <div><img src="https://dogecoin.com/imgs/doge.png" alt="Smiley face"/></div>
       <p>Such wow. Very <Link to="//">React</Link>.</p>
-      <ul>
-        <h3>Users</h3>
-        {data.allMongodbTestdbUsers.edges.map(({ node }, index) => (
-          <li>{node.username}</li>
-        ))}
-      </ul>
+
     </Layout>
   )
 
