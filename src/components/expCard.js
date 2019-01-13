039db0137813f1c0d4b1b0365c145b8c4924c68e
abalarin <abalarin@outlook.com>
2019-01-13 13:55:22 -0500

    reactjs

---
+++ src/components/expCard.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+class experienceCard extends React.Component {
+  render() {
+    return (
+      <div class="row">
+        <div class="col">
+          <h1> Resume Card Img </h1>
+        </div>
+        <div class="col">
+          <h1> Resume Card Description </h1>
+          <p> You can programmatically create pages using “page template components”. All pages are React components but very often these components are just wrappers around data from files or other sources.
+
+</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default experienceCard
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import github from "../../public/IMG_4982.png"
 
-export default ( ) => (
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+export default () => (
   <Layout>
     <h1>Patrick Star on Wumbo </h1>
     <div>
@@ -20,12 +26,61 @@ export default ( ) => (
         </p>
       </blockquote>
     </div>
-
     <div>
-      <div><img src="https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/0/0c/Wumbo.gif/revision/latest?cb=20130201021644" alt="Smiley face"/></div>
+      <img src="https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/0/0c/Wumbo.gif/revision/latest?cb=20130201021644" alt="Smiley face"/>
+    </div>
+    <div>
       <button class="primary-button">
-      <Link to="/profile/"> Click me </Link>
+        <Link to="/profile/"> Click me </Link>
       </button>
+    </div>
+
+    <div class="hr-divider">
+      <h2 class="hr-divider-content hr-divider-heading">
+        Experiences
+      </h2>
+    </div>
+
+
+    <div class="row">
+      <div class="col">
+        <img src={github} alt="Logo" />
+      </div>
+
+      <div class="col">
+        <h3> This Thing </h3>
+        <p>
+          You know, I wumbo, You wumbo, He she me wumbo, wumbo, Wumboing,
+          We'll have thee wumbo, Wumborama, Wumbology, The study of wumbo?
+        </p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <h3> Github </h3>
+
+        <p>
+          First, we set up the component’s state with a theme property and initialized it to null. Next, we define a chooseTheme() click event handler for choosing a theme or resetting the theme.
+        </p>
+      </div>
+      <div class="col">
+        <img src={github} alt="Logo" />
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <img src={github} alt="Logo" />
+      </div>
+
+      <div class="col">
+        <h3> The other Title</h3>
+        <p>
+          You know, I wumbo, You wumbo, He she me wumbo, wumbo, Wumboing,
+          We'll have thee wumbo, Wumborama, Wumbology, The study of wumbo?
+        </p>
+      </div>
     </div>
   </Layout>
 )
