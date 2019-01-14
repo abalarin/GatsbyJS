c02782ea77efeb032a913581a42d6462e92588bc
abalarin <abalarin@outlook.com>
2019-01-14 13:32:48 -0500

    images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
-import github from "../../public/IMG_4982.png"
+import github from "./IMG_4982.png"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
